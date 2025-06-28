4099c5915b5d1467bda50342d689b45adcbb0c42
Xurshiddd <marifovxurshid800@gmail.com>
2025-06-28 15:40:47 +0500

    asdasd

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,6 @@ use Laravel\Socialite\Facades\Socialite;
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 Route::middleware('auth')->group(function () {
-//    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::get('/profile', [StudentController::class, 'profile'])->name('profile');
     Route::post('/profile', [StudentController::class, 'updateProfile'])->name('updateProfile');
 });
